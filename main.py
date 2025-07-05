389b75494d63e46fad215f430b7e9d4f7468ffdf
Teja Laveti <tejalaveti23@gmail.com>
2025-07-05 10:28:12 +0530

    Data Validation complete

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import (TrainingPipelineConfig,
+                                                  DataIngestionConfig,
+                                                  DataValidationConfig)
 
 import sys
 from datetime import datetime
@@ -13,7 +16,14 @@ if __name__=="__main__":
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        datavalidataionconfig = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, datavalidataionconfig)
+        logging.info("Initiate data validation")
+        datavalidationartifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(datavalidationartifact)
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -12,6 +12,8 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH: str = os.path.join("data_schema", "schema.yaml")
+
 # Data Ingestion related constants start with DATA_INGESTION VAR NAME
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "TEJALAVETI"
@@ -19,3 +21,10 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+# Data Validation related constants start with DATA_VALIDATION VAR NAME
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
