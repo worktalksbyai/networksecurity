13a95dab6f261a921ea32497cad2e757705a05f2
Teja Laveti <tejalaveti23@gmail.com>
2025-07-05 07:18:56 +0530

    Data Ingestion Phase Completed

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 
 import sys
+from datetime import datetime
 
 if __name__=="__main__":
     try:
-        trainingpipelineconfig = TrainingPipelineConfig
+        trainingpipelineconfig = TrainingPipelineConfig(datetime.now())
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
